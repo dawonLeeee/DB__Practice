bf598c8cff03e0129b3f6777614393b784e8d78d
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-09-14 15:56:40 +0900

    learn SUBQUERY, DDL

--- OracleDB/07_SUBQUERY.sql
+++ OracleDB/07_SUBQUERY.sql
@@ -22,7 +22,7 @@ FROM EMPLOYEE e
 WHERE DEPT_CODE = 'D9';
 
 -- 3) 부서코드가 노옹철사원과 같은 소속의 직원 명단 조회   
---> 위의 2개의 단계를 하나의 쿼리로!!! --> 1) 쿼리문을 서브쿼리로!!
+--> 위의 2개의 단계를 하나의 쿼리로!!! --> 1) 쿼리문을 서브쿼리로!
 SELECT EMP_NAME, DEPT_CODE
 FROM EMPLOYEE e 
 WHERE DEPT_CODE = (SELECT DEPT_CODE
@@ -113,14 +113,15 @@ WHERE EMP_NAME = '노옹철');
 -- 부서별(부서가 없는 사람 포함) 급여의 합계 중 가장 큰 부서의
 -- 부서명, 급여 합계를 조회 
 SELECT DEPT_TITLE, SUM(SALARY)
-FROM EMPLOYEE e 
-LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT ON(DEPT_ID = DEPT_CODE)
 GROUP BY DEPT_TITLE
-WHERE SUM(SALARY) = (
-	SELECT MAX(SUM(SALARY))
-	FROM EMPLOYEE e2 
+HAVING SUM(SALARY) = (
+	SELECT MAX(SUM(SALARY)) 
+	FROM EMPLOYEE
 	GROUP BY DEPT_CODE
 );
+
 
 	
 
@@ -169,6 +170,7 @@ GROUP BY DEPT_CODE);
 
 -- 부서별 최고 급여를 받는 직원의 
 -- 이름, 직급, 부서코드, 급여를 부서 순으로 정렬하여 조회
+
 SELECT EMP_NAME, JOB_CODE , DEPT_CODE , SALARY 
 FROM EMPLOYEE
 GROUP BY DEPT_CODE 
@@ -182,6 +184,21 @@ ORDER BY DEPT_CODE;
 
 -- 사수에 해당하는 직원에 대해 조회 (사수 = MANAGER_ID 컬럼에 작성된 사람)
 --  사번, 이름, 부서명, 직급명, 구분(사수 / 직원)
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME, 
+	CASE WHEN EMP_ID IN
+	(SELECT DISTINCT MANAGER_ID
+		FROM EMPLOYEE e
+		WHERE MANAGER_ID IS NOT NULL) THEN '사수' 
+	ELSE '사원'
+	END 구분
+FROM EMPLOYEE e
+NATURAL JOIN JOB
+LEFT JOIN DEPARTMENT d ON(DEPT_CODE = DEPT_ID)
+ORDER BY EMP_ID;
+
+
+
+
 
 -- 1) 사수에 해당하는 사원 번호 조회
 SELECT DISTINCT MANAGER_ID
@@ -255,6 +272,10 @@ WHERE MANAGER_ID IS NOT NULL);
 
 -- 대리 직급의 직원들 중에서 과장 직급의 최소 급여보다 많이 받는 직원의
 -- 사번, 이름, 직급, 급여를 조회하세요
+
+
+
+
 -- 단, > ANY 혹은 < ANY 연산자를 사용하세요
 
 -- > ANY, < ANY : 여러개의 결과값 중에서 하나라도 큰 / 작은 경우
@@ -297,7 +318,6 @@ NATURAL JOIN JOB j
 WHERE JOB_NAME = '과장');
 
 
-
 -- 차장 직급의 급여의 가장 큰 값보다 많이 받는 과장 직급의 직원
 -- 사번, 이름, 직급, 급여를 조회하세요
 -- 단, > ALL 혹은 < ALL 연산자를 사용하세요
@@ -317,7 +337,7 @@ WHERE JOB_NAME = '과장'
 	FROM EMPLOYEE e 
 	NATURAL JOIN JOB
 	WHERE JOB_NAME = '차장');
-       
+
 -- 방법2(MAX) 
 SELECT EMP_ID, EMP_NAME, JOB_NAME, SALARY
 FROM EMPLOYEE e 
@@ -328,6 +348,7 @@ WHERE JOB_NAME = '과장'
 	FROM EMPLOYEE e 
 	NATURAL JOIN JOB
 	WHERE JOB_NAME = '차장');
+
                 
 
 -- 서브쿼리 중첩 사용(응용편!)
@@ -336,6 +357,8 @@ WHERE JOB_NAME = '과장'
 -- LOCATION 테이블에서 NATIONAL_CODE가 KO인 경우의 LOCAL_CODE와
 -- DEPARTMENT 테이블의 LOCATION_ID와 동일한 DEPT_ID가 
 -- EMPLOYEE테이블의 DEPT_CODE와 동일한 사원을 구하시오.
+
+
 
 -- 1) LOCATION 테이블을 통해 NATIONAL_CODE가 KO인 LOCAL_CODE 조회
 SELECT LOCAL_CODE 
@@ -369,6 +392,7 @@ WHERE DEPT_CODE IN
 
 -- 퇴사한 여직원과 같은 부서, 같은 직급에 해당하는
 -- 사원의 이름, 직급, 부서, 입사일을 조회        
+	
 
 -- 1) 퇴사한 여직원 조회
 SELECT EMP_NAME
@@ -404,6 +428,7 @@ AND SUBSTR(EMP_NO, 8, 1) = '2');
 -------------------------- 연습문제 -------------------------------
 -- 1. 노옹철 사원과 같은 부서, 같은 직급인 사원을 조회하시오. (단, 노옹철 사원은 제외)
 --    사번, 이름, 부서코드, 직급코드, 부서명, 직급명
+
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, JOB_CODE, DEPT_TITLE, JOB_NAME
 FROM EMPLOYEE e 
 LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
@@ -417,6 +442,7 @@ AND EMP_NAME <> '노옹철';
 
 -- 2. 2000년도에 입사한 사원의 부서와 직급이 같은 사원을 조회하시오
 --    사번, 이름, 부서코드, 직급코드, 고용일
+
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, HIRE_DATE 
 FROM EMPLOYEE e 
 WHERE (DEPT_CODE, JOB_CODE) IN (
@@ -428,6 +454,8 @@ WHERE (DEPT_CODE, JOB_CODE) IN (
 
 -- 3. 77년생 여자 사원과 동일한 부서이면서 동일한 사수를 가지고 있는 사원을 조회하시오
 --    사번, 이름, 부서코드, 사수번호, 주민번호, 고용일     
+
+
 SELECT EMP_ID, EMP_NAME, DEPT_CODE, MANAGER_ID, EMP_NO, HIRE_DATE  
 FROM EMPLOYEE e 
 WHERE (DEPT_CODE, MANAGER_ID) IN (
@@ -445,6 +473,7 @@ WHERE (DEPT_CODE, MANAGER_ID) IN (
 -- 본인 직급의 평균 급여를 받고 있는 직원의
 -- 사번, 이름, 직급, 급여를 조회하세요
 -- 단, 급여와 급여 평균은 만원단위로 계산하세요 TRUNC(컬럼명, -4)    
+
 
 -- 1) 급여를 200, 600만 받는 직원 (200만, 600만이 평균급여라 생각 할 경우)
 SELECT EMP_ID, EMP_NAME, JOB_CODE, SALARY 
@@ -498,6 +527,7 @@ WHERE SALARY > (SELECT AVG(SALARY) FROM EMPLOYEE SUB
 --    사번, 이름, 부서명(NULL이면 '소속없음'), 직급명, 입사일을 조회하고
 --    입사일이 빠른 순으로 조회하세요
 --    단, 퇴사한 직원은 제외하고 조회하세요
+
 SELECT EMP_ID, EMP_NAME, NVL(DEPT_TITLE, '소속없음') "부서명", JOB_NAME, HIRE_DATE 
 FROM EMPLOYEE MAIN
 LEFT JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
@@ -548,6 +578,7 @@ FROM EMPLOYEE E1 ;
 -- 단 관리자가 없는 경우 '없음'으로 표시
 -- (스칼라 + 상관 쿼리)
 --> SELECT 절에 작성되는 단일행 서브쿼리
+
 SELECT EMP_ID, EMP_NAME, MANAGER_ID,
 NVL((SELECT EMP_NAME FROM EMPLOYEE SUB WHERE SUB.EMP_ID = MAIN.MANAGER_ID), '없음') 관리자명
 FROM EMPLOYEE MAIN;
@@ -574,6 +605,7 @@ WHERE 부서 = '기술지원부';
 -- 전 직원 중 급여가 높은 상위 5명의
 -- 순위, 이름, 급여 조회
 
+
 -- ROWNUM : 행 번호를 나타내는 가상 컬럼
 --  --> SELECT/WHERE/ORDER BY절 사용 가능
 SELECT ROWNUM, EMP_NAME, SALARY 
@@ -597,6 +629,15 @@ WHERE ROWNUM <= 5;
 
 -- 급여 평균이 3위 안에 드는 부서의 부서코드와 부서명, 평균급여를 조회
 -- ROWNUM 생략 가능
+
+SELECT DEPT_CODE, DEPT_TITLE, 평균급여
+SELECT DEPT_CODE, DEPT_TITLE, CEIL(AVG(SALARY)) 평균급여 FROM EMPLOYEE e 
+JOIN DEPARTMENT d ON(DEPT_ID = DEPT_CODE)
+GROUP BY DEPT_CODE, DEPT_TITLE ;
+
+
+
+
 SELECT /*ROWNUM,*/ DEPT_CODE, DEPT_TITLE, 평균급여
 FROM (
 	SELECT DEPT_CODE, DEPT_TITLE, CEIL(AVG(SALARY)) 평균급여
